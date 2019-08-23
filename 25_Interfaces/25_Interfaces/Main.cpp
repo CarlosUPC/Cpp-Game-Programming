cf3f398a74bc64f1e7f53c5fb049d769adfb38e5
CarlosUPC <36167348+CarlosUPC@users.noreply.github.com>
2019-08-23 21:29:34 +0200

    interfaces or pure virtual functions

---
+++ 25_Interfaces/25_Interfaces/Main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+
+class Printable
+{
+public:
+	virtual std::string GetClassName() = 0;
+
+};
+class Entity: public Printable
+{
+public:
+	virtual std::string GetName() { return "Entity"; } 
+
+ std::string GetClassName() override { return "Entity"; }
+
+};
+
+class Player : public Entity {
+
+private:
+	std::string m_Name;
+public:
+	Player(const std::string& name)
+		:m_Name(name) {}
+
+	std::string GetName() override { return m_Name; } 
+
+	std::string GetClassName() override { return "Player"; }
+
+};
+
+void PrintName(Entity* entity) {
+	std::cout << entity->GetName() << std::endl;
+}
+
+class A : public Printable
+{
+public:
+	std::string GetClassName() override { return "A"; }
+};
+
+void Print(Printable* obj) {
+	std::cout << obj->GetClassName() << std::endl;
+}
+int main()
+{
+	Entity* e = new Entity();
+	//PrintName(e);
+	//std::cout << e->GetName() << std::endl;
+	Print(e);
+	
+	Player* p = new Player("Carlos");
+	//PrintName(p);
+	//std::cout << p->GetName() << std::endl;
+	Print(p);
+
+	Print(new A());
+	//Entity* entity = p;
+	//std::cout << entity->GetName() << std::endl;
+
+	std::cin.get();
+}
